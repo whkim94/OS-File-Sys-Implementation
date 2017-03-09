d766b5a2515298cf25b8b13f99b510bba478ab6c
jlee1819 <janzen1819@gmail.com>
2017-03-08 16:09:35 -0800

    Updated fs_info()
    -Changed all malloc to calloc
    -fs_info() compiles, but not sure if the implementation is correct

--- libuthread/fs.c
+++ libuthread/fs.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 #define _UTHREAD_PRIVATE
 #include "disk.h"
 #include "fs.h"
+
 
 struct superblock{
 	uint64_t signiture;
@@ -35,8 +37,21 @@ struct superblock* mysuperblock;
 struct FATEntry* myFAT;
 void* myData;
 
+
+
 int fs_mount(const char *diskname)
 {
+	/**
+ * fs_mount - Mount a file system
+ * @diskname: Name of the virtual disk file
+ *
+ * Open the virtual disk file @diskname and mount the file system that it
+ * contains. A file system needs to be mounted before files can be read from it
+ * with fs_read() or written to it with fs_write().
+ *
+ * Return: -1 if virtual disk file @diskname cannot be opened, or if no valid
+ * file system can be located. 0 otherwise.
+ */
 	if(block_disk_open(diskname) == -1){
 		printf("%s Diskname failed to open\n", diskname);
 		return -1;
@@ -44,27 +59,19 @@ int fs_mount(const char *diskname)
 
 	//reading superblock
 	mysuperblock = malloc(sizeof(struct superblock));
-
 	block_read(0,mysuperblock);
-
-
     //reading FAT
 	int FATSize = mysuperblock->numFAT;
-
-	myFAT = malloc(sizeof(struct FATEntry) * FATSize);
+	myFAT = calloc(sizeof(struct FATEntry), FATSize);
 	for (int i=0;i<FATSize;i++) {
 		block_read(1+i,myFAT+(i*4096));	
 	}
-	
 	//reading RootDirectory
-	myRootDirectory = malloc(sizeof(struct rootDirectory));
+	myRootDirectory = calloc(sizeof(struct rootDirectory), 128);
 	int rootPos = 1+FATSize;
 	block_read(rootPos,myRootDirectory);
-
-
 	//reading data
 	int dataSize = mysuperblock->numData;
-	
 	myData = calloc(dataSize,4096);
 	int dataPos = rootPos + 1;
 	for (int i=0;i<dataSize;i++) {
@@ -76,6 +83,16 @@ int fs_mount(const char *diskname)
 
 int fs_umount(void)
 {
+	/**
+ * fs_umount - Unmount file system
+ *
+ * Unmount the currently mounted file system and close the underlying virtual
+ * disk file.
+ *
+ * Return: -1 if no underlying virtual disk was opened, or if the virtual disk
+ * cannot be closed, or if there are still open file descriptors. 0 otherwise.
+ */
+
 	free(mysuperblock);
 	free(myFAT);
 	free(myRootDirectory);
@@ -88,6 +105,15 @@ int fs_umount(void)
 
 int fs_info(void)
 {
+	/**
+ * fs_info - Display information about file system
+ *
+ * Display some information about the currently mounted file system.
+ *
+ * Return: -1 if no underlying virtual disk was opened. 0 otherwise.
+ */
+
+
 	printf("FS Info:\n");
 	printf("total_blk_count=%d\n", mysuperblock->numBlocks);
 	printf("fat_blk_count=%d\n", mysuperblock->numFAT);
@@ -96,23 +122,85 @@ int fs_info(void)
 	int dataBlock = rdirBlock + 1;
 	printf("data_blk=%d\n", dataBlock);
 	printf("data_blk_count=%d\n",mysuperblock->numData);
-	printf("fat_free_ratio=%d/%d\n", 199,200);
-	printf("rdir_free_ratio=%d/%d\n", 128,128);
+
+	int FATCount = 1;
+	//printf("First entry in myFAT is: %d\n",myFAT->data[FATCount]);
+	while (myFAT->data[FATCount] != 0) {
+		FATCount++;
+	}
+	printf("fat_free_ratio=%d/%d\n", mysuperblock->numData-FATCount, mysuperblock->numData);
+
+	int RDCount = 0, comp = 0;
+	//comp = strcmp( (char)*(myRootDirectory + count)->fileName, "\0");
+	printf("First entry in myRootDirectory is: %s\n", *(myRootDirectory+RDCount*32)->fileName);
+	while ( *(myRootDirectory+RDCount*32)->fileName != 0 ) {
+		RDCount++;
+	}
+	printf("rdir_free_ratio=%d/%d\n", 128 - RDCount ,128);
+
+	return 0;
 }
 
 int fs_create(const char *filename)
 {
-	/* TODO: PART 3 - Phase 2 */
+/**
+ * fs_create - Create a new file
+ * @filename: File name
+ *
+ * Create a new and empty file named @filename in the root directory of the
+ * mounted file system. String @filename must be NULL-terminated and its total
+ * length cannot exceed %FS_FILENAME_LEN characters (including the NULL
+ * character).
+ *
+ * Return: -1 if a file named @filename already exists, or if string @filename
+ * is too long, or if the root directory already contains %FS_FILE_MAX_COUNT
+ * files. 0 otherwise.
+ */
+
+	//if(myRootDirectory == FS_FILE_MAX_COUNT)
+		//return -1;
+
+	// struct node* newEle = (struct node*)malloc(sizeof(struct node));
+	// newEle->ndata = data;
+	// newEle->next = NULL;
+	// if (head == NULL)
+	// 	head = newEle;
+	// else
+	// 	tail->next = newEle;
+	// tail = newEle;
+
+
+	// struct rootDirectory* thisfile = (struct rootDirectory*)malloc(sizeof(struct rootDirectory));
+	// thisfile->fileName = filemame;
+	// thisfile->fileSize = 0;
+	// thisfile->fileIndex = 69;
+
+
 }
 
 int fs_delete(const char *filename)
 {
-	/* TODO: PART 3 - Phase 2 */
+/**
+ * fs_delete - Delete a file
+ * @filename: File name
+ *
+ * Delete the file named @filename from the root directory of the mounted file
+ * system.
+ *
+ * Return: -1 if there is no file named @filename to delete, or if file
+ * @filename is currently open. 0 otherwise.
+ */
 }
 
 int fs_ls(void)
 {
-	/* TODO: PART 3 - Phase 2 */
+/**
+ * fs_ls - List files on file system
+ *
+ * List information about the files located in the root directory.
+ *
+ * Return: -1 if no underlying virtual disk was opened. 0 otherwise.
+ */
 }
 
 int fs_open(const char *filename)
